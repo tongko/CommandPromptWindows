6ce9585a6205132d38e565806c74cb48df8b535c
Terry Liew <tongko.liew@gmail.com>
2014-04-15 00:46:23 +0800

    WriteConsoleOutput not working, reason unknown.

--- ConsolePlayground/ConsolePlayground.cpp
+++ ConsolePlayground/ConsolePlayground.cpp
@@ -13,134 +13,120 @@ void WriteChar(CHAR_INFO info[], int x, int y, TCHAR *chs, int len) {
 	for (int i = 0; i < len; i++) {
 		ci = &info[y * WIDTH + xpos];
 		ci->Char.UnicodeChar = chs[i];
-		ci->Attributes = FOREGROUND_GREEN | FOREGROUND_RED;
+		ci->Attributes = FOREGROUND_RED | FOREGROUND_INTENSITY;
 		xpos++;
 	}
 
 }
 
-template <typename T> struct Test {
-	T * prev;
-	T * next;
-};
-
-typedef struct _tagMSG {
-	INT			hWindow;
-	UINT        message;
-	WPARAM      wParam;
-	LPARAM      lParam;
-	DWORD       time;
-	POINT       pt;
-} MESSAGE, FAR *LPMESSAGE;
-
-Test<MESSAGE> * g_test;
-
-void Func() {
-	LPMESSAGE lpMsg = new MESSAGE();
-	lpMsg->hWindow = 123;
-	g_test->next = lpMsg;
-	lpMsg = new MESSAGE();
-	lpMsg->hWindow = 456;
-	g_test->prev = lpMsg;
-}
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-	g_test = new Test<MESSAGE>;
-	Func();
+	HANDLE hNewScreenBuffer = CreateConsoleScreenBuffer(
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		NULL,
+		CONSOLE_TEXTMODE_BUFFER,
+		NULL);
+	if (hNewScreenBuffer == INVALID_HANDLE_VALUE) {
+		printf("Create console screen buffer failed - (%d)\n", GetLastError());
+		Sleep(10000);
+		return 1;
+	}
 
-	printf("next: %d\nprev: %d\n", g_test->next->hWindow, g_test->prev->hWindow);
-	Sleep(10000);
-	//HANDLE _stdin = GetStdHandle(STD_INPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hNewScreenBuffer, &csbi);
+	COORD size = csbi.dwSize;
+	int length = size.X * size.Y;
+	PCHAR_INFO ci = new CHAR_INFO[length];
+	for (int i = 0; i < length; i++)
+	{
+		ci[i].Attributes = FOREGROUND_RED | FOREGROUND_INTENSITY;
+		ci[i].Char.UnicodeChar = TEXT(' ');
+	}
+
+	WriteConsoleOutput(hNewScreenBuffer, ci, csbi.dwSize, csbi.dwCursorPosition, &csbi.srWindow);
+
+	if (!SetConsoleActiveScreenBuffer(hNewScreenBuffer)) {
+		printf("Set console screen buffer failed - (%d)\n", GetLastError());
+		Sleep(10000);
+		return 1;
+	}
+
+	HANDLE _stdin = GetStdHandle(STD_INPUT_HANDLE);
 	//HANDLE _stdout = GetStdHandle(STD_OUTPUT_HANDLE);
-	////HANDLE hNewScreenBuffer = CreateConsoleScreenBuffer(
-	////	GENERIC_READ | GENERIC_WRITE,
-	////	FILE_SHARE_READ | FILE_SHARE_WRITE,
-	////	NULL,
-	////	CONSOLE_TEXTMODE_BUFFER,
-	////	NULL);
-	////if (_stdout == INVALID_HANDLE_VALUE || hNewScreenBuffer == INVALID_HANDLE_VALUE) {
-	////	printf("Create console screen buffer failed - (%d)\n", GetLastError());
-	////	Sleep(10000);
-	////	return 1;
-	////}
 
-	///*if (!SetConsoleActiveScreenBuffer(hNewScreenBuffer)) {
-	//	printf("Set console screen buffer failed - (%d)\n", GetLastError());
-	//	Sleep(10000);
-	//	return 1;
-	//}*/
+	SMALL_RECT srctReadRect = { 1, 1, 30, 4 };
+	SMALL_RECT srctWriteRect = { 0, 0, 29, 3 };
+	CHAR_INFO chiBuffer[WIDTH * HEIGHT]; // [4][30]; 
+	COORD coordBufSize;
+	COORD coordBufCoord;
+	BOOL fSuccess;
 
-	//SMALL_RECT srctReadRect = { 1, 1, 30, 4 };
-	//SMALL_RECT srctWriteRect = { 0, 0, 29, 3 };
-	//CHAR_INFO chiBuffer[WIDTH * HEIGHT]; // [4][30]; 
-	//COORD coordBufSize;
-	//COORD coordBufCoord;
-	//BOOL fSuccess;
+	coordBufSize.Y = 4;
+	coordBufSize.X = 30;
 
-	//coordBufSize.Y = 4;
-	//coordBufSize.X = 30;
+	coordBufCoord.X = coordBufCoord.Y = 0;
 
-	//coordBufCoord.X = coordBufCoord.Y = 0;
+	if (!ReadConsoleOutput(
+		hNewScreenBuffer,
+		chiBuffer,
+		coordBufSize,
+		coordBufCoord,
+		&srctReadRect)) {
+		printf("Read console output failed - (%d)\n", GetLastError());
+		Sleep(10000);
+		return 1;
+	}
 
-	//if (!ReadConsoleOutput(
-	//	_stdout,
-	//	chiBuffer,
-	//	coordBufSize,
-	//	coordBufCoord,
-	//	&srctReadRect)) {
-	//	printf("Read console output failed - (%d)\n", GetLastError());
-	//	Sleep(10000);
-	//	return 1;
-	//}
+	WriteChar(chiBuffer, 0, 0, TEXT("Mouse POS X: "), 13);
+	WriteChar(chiBuffer, 0, 1, TEXT("Mouse POS Y: "), 13);
+	if (!WriteConsoleOutput(hNewScreenBuffer, chiBuffer, coordBufSize, coordBufCoord, &srctWriteRect)) {
+		printf("Write console input failed - (%d)\n", GetLastError());
+		Sleep(10000);
+		return 1;
+	}
 
-	//WriteChar(chiBuffer, 0, 0, TEXT("Mouse POS X: "), 13);
-	//WriteChar(chiBuffer, 0, 1, TEXT("Mouse POS Y: "), 13);
-	//if (!WriteConsoleOutput(_stdout, chiBuffer, coordBufSize, coordBufCoord, &srctWriteRect)) {
-	//	printf("Write console input failed - (%d)\n", GetLastError());
-	//	Sleep(10000);
-	//	return 1;
-	//}
+	BOOL stop = FALSE;
+	INPUT_RECORD ir[120];
+	DWORD read;
 
-	//BOOL stop = FALSE;
-	//INPUT_RECORD ir[120];
-	//DWORD read;
+	while (!stop)
+	{
+		if (!ReadConsoleInput(_stdin, ir, 120, &read)) {
+			printf("Read console input failed - (%d)\n", GetLastError());
+			Sleep(10000);
+			return 1;
+		}
+		
+		for (DWORD i = 0; i < read; i++)
+		{
+			switch (ir[i].EventType)
+			{
+			case KEY_EVENT:
+				if (ir[i].Event.KeyEvent.wVirtualKeyCode == VK_ESCAPE) {
+					stop = TRUE;
+				}
+				break;
+			case MOUSE_EVENT:
+				COORD pos = ir[i].Event.MouseEvent.dwMousePosition;
+				TCHAR s[4];
+				_itow_s(pos.X, s, 10);
+				int count = _tcslen(s);
+				WriteChar(chiBuffer, 13, 0, s, count);
+				_itow_s(pos.Y, s, 10);
+				count = _tcslen(s);
+				WriteChar(chiBuffer, 13, 1, s, count);
 
-	//while (!stop)
-	//{
-	//	if (!ReadConsoleInput(_stdin, ir, 120, &read)) {
-	//		printf("Read console input failed - (%d)\n", GetLastError());
-	//		Sleep(10000);
-	//		return 1;
-	//	}
-	//	
-	//	for (DWORD i = 0; i < read; i++)
-	//	{
-	//		switch (ir[i].EventType)
-	//		{
-	//		case KEY_EVENT:
-	//			if (ir[i].Event.KeyEvent.wVirtualKeyCode == VK_ESCAPE) {
-	//				stop = TRUE;
-	//			}
-	//			break;
-	//		case MOUSE_EVENT:
-	//			COORD pos = ir[i].Event.MouseEvent.dwMousePosition;
-	//			TCHAR s[4];
-	//			_itow_s(pos.X, s, 10);
-	//			int count = _tcslen(s);
-	//			WriteChar(chiBuffer, 13, 0, s, count);
-	//			_itow_s(pos.Y, s, 10);
-	//			count = _tcslen(s);
-	//			WriteChar(chiBuffer, 13, 1, s, count);
+				if (!WriteConsoleOutput(hNewScreenBuffer, chiBuffer, coordBufSize, coordBufCoord, &srctWriteRect)) {
+					printf("Write console input failed - (%d)\n", GetLastError());
+					Sleep(10000);
+					return 1;
+				}
+			}
+		}
+	}
 
-	//			if (!WriteConsoleOutput(_stdout, chiBuffer, coordBufSize, coordBufCoord, &srctWriteRect)) {
-	//				printf("Write console input failed - (%d)\n", GetLastError());
-	//				Sleep(10000);
-	//				return 1;
-	//			}
-	//		}
-	//	}
-	//}
+	CloseHandle(hNewScreenBuffer);
 
 	return 0;
 }
